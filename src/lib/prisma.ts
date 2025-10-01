8e4941be9fee97b2fe5c4c2554d509f787592227
Mbiybee Prince-Charles Berinyuy <mbiyber@outlook.com>
2025-10-01 17:37:55 +0100

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,12 @@
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ log: ["error", "warn"] });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error", "warn"], // optional
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
